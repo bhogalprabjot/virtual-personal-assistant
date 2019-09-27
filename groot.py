e20e4de96f676d9ec257aa6d74d4480171ee8aa3
bhogalprabjot <bhogalprabjot1@gmail.com>
2019-09-27 20:20:13 +0530

    update

--- groot.py
+++ groot.py
@@ -1,10 +1,10 @@
 import pyttsx3
 import datetime
-
+import speech_recognition as sr
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-for i in range(0,20):
-	print(voices[i].id)
+# for i in range(0,20):
+    # print(voices[i].id)
 engine.setProperty('voice', voices[16].id)
 
 def speak(audio):
@@ -13,7 +13,6 @@ def speak(audio):
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-
     if hour >= 0 and hour < 12:
         speak("Good Morning!")
 
@@ -25,5 +24,28 @@ def wishMe():
 
     speak("I am Groot! How may I help you?")
 
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source) 
+        print("Listening...")
+        r.pause_threshold = 1
+        r.energy_threshold = 100
+        audio = r.listen(source)
+
+    try:
+        print("Recoginzing...")
+        query = r.recognize_google(audio, Language = 'en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print(e)
+        print("Say that again please...")
+        return "None"
+
+    return query
+
+
 if __name__ == "__main__":
     wishMe()
+    takeCommand()
