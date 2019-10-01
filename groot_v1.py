92ebfeaa60a932cc8aa8e2e0c05776d2aa93ac45
bhogalprabjot <bhogalprabjot1@gmail.com>
2019-10-01 19:06:33 +0530

    update

--- groot_v1.py
+++ groot_v1.py
@@ -3,6 +3,7 @@ from playsound import playsound
 import datetime
 import webbrowser
 import wikipedia
+import json, codecs, apiai
 import speech_recognition as sr
 
 def speak(mytext):
@@ -35,42 +36,57 @@ def takeCommand():
 
     try:
         print("Recoginzing...")
-        query = r.recognize_google(audio, language = 'en-in')
-        print(f"User said: {query}\n")
+        user_query = r.recognize_google(audio, language = 'en-in')
+        print(f"User said: {user_query}\n")
 
     except Exception as e:
         print(e)
         print("Say that again please...")
         return "None"
 
-    return query
+    return user_query
+
 
 if __name__ == "__main__":
     wishMe()
-    while True:
-        query = takeCommand().lower()
 
-        if 'what is' in query:
+    CLIENT_ACCESS_TOKEN = "7f790c9c5d11467493162773c9196204 "
+    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+    request = ai.text_request()
+    request.lang = 'en '
+    request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+
+    while True:
+        user_query = takeCommand().lower()
+
+        if 'what is' in user_query:
             print("Searching wikipedia...")
-            query = query.replace("what is", "")
-            results = wikipedia.summary(query, sentences=2)
+            user_query = user_query.replace("what is", "")
+            results = wikipedia.summary(user_query, sentences=2)
             speak("According to Wikipedi...")
             # speak("Baba Bole...")
             print(results)
             speak(results)
 
-        elif 'open youtube' in query:
+        elif 'open youtube' in user_query:
             print("Opening YouTube...")
             webbrowser.open_new_tab("https://www.youtube.com/")
 
-        elif 'open github' in query:
+        elif 'open github' in user_query:
             print("Opening GitHub...")
             webbrowser.open_new_tab("https://github.com/")
 
-        elif 'open google' in query:
+        elif 'open google' in user_query:
             print("Opening Google...")
             webbrowser.open_new_tab("https://www.google.com/")
 
-        elif 'the time' in query:
+        elif 'the time' in user_query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Sir, the time is {strTime}")
+
+        else:
+            request.query = user_query
+            response = request.getresponse()
+            obj = json.load(response)
+            reply = obj['result']['fulfillment']['speech']
+            speak(reply)
